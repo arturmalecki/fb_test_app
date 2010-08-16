78038ae22a5fe058b035b87ced1d7434b63f9d3f
Artur Małecki <artur.malecki@aenima.pl>
2010-08-16 08:44:31 +0200

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,12 @@ role :web, "83.19.4.130"
 role :app, "83.19.4.130"
 role :db,  "83.19.4.130", :primary => true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-#
 task :after_update_code do
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
+end
+
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 end
